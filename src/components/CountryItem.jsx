734cb9a37fb213e3cdbff3528aba00e6a99dca5b
ThiagoAndo <ando.thiago@gmail.com>
2024-06-21 21:25:25 +0100

    Clock logick OK

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 function CountryItem({ country }) {
-
+console.log(country)
   return (
     <Link to={`/${country.name.common}`}>
       <article className="dark" id={country.name.common}>
--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-const time = new Date("2024-06-13T00:45:45.751884+01:00");
+let time = null;
 // let d = new Date("2024-06-13T17:37:58.751884+01:00");
 // d.setSeconds(d.getSeconds() + 1);
 // d.setSeconds(d.getSeconds() + 1);
@@ -15,63 +15,66 @@ async function loadTime(region, name, capital) {
   if (!response.ok) {
   } else {
     const resData = await response.json();
-    return resData;
+    console.log(resData);
+    time = new Date(resData.datetime);
   }
 }
 
 function Clock({ region, name, capital }) {
-  console.log(region + " " +name)
+  console.log(region + " " + name+" " + capital);
   const [timer, setTimer] = useState(time);
 
-  let inter = useRef();
+  // let inter = useRef();
 
-  const startIt = () => {
-    clearInterval(inter.current);
-    inter.current = setInterval(() => {
-      setTimer(time.setSeconds(time.getSeconds() + 1));
-    }, 1000);
-  };
+  // const startIt = () => {
+  //   clearInterval(inter.current);
+  //   inter.current = setInterval(() => {
+  //     setTimer(time.setSeconds(time.getSeconds() + 1));
+  //   }, 1000);
+  // };
+  // startIt();
 
-  console.log(time.getSeconds());
-  console.log("time.getSeconds())");
-
-  startIt();
-  return (
-    <>
-      <div className="clock">
-        <div
-          className="hour_hand"
-          style={{
-            transform: `rotateZ(${time.getHours() * 30}deg)`,
-          }}
-        />
-        <div
-          id="min_hand"
-          style={{
-            transform: `rotateZ(${time.getMinutes() * 6}deg)`,
-          }}
-        />
-        <div
-          className="sec_hand"
-          style={{
-            transform: `rotateZ(${time.getSeconds() * 6}deg)`,
-          }}
-        />
-        <span className="twelve">12</span>
-        <span className="one">1</span>
-        <span className="two">2</span>
-        <span className="three">3</span>
-        <span className="four">4</span>
-        <span className="five">5</span>
-        <span className="six">6</span>
-        <span className="seven">7</span>
-        <span className="eight">8</span>
-        <span className="nine">9</span>
-        <span className="ten">10</span>
-        <span className="eleven">11</span>
-        <h3>{capital}</h3>
-      </div>
-    </>
+  useEffect(() => {
+    if (time === null) {
+      loadTime(region, name, capital);
+    }
+  }, []);
+  return (<h1>clock</h1>
+    // <>
+    //   <div className="clock">
+    //     <div
+    //       className="hour_hand"
+    //       style={{
+    //         transform: `rotateZ(${time.getHours() * 30}deg)`,
+    //       }}
+    //     />
+    //     <div
+    //       id="min_hand"
+    //       style={{
+    //         transform: `rotateZ(${time.getMinutes() * 6}deg)`,
+    //       }}
+    //     />
+    //     <div
+    //       className="sec_hand"
+    //       style={{
+    //         transform: `rotateZ(${time.getSeconds() * 6}deg)`,
+    //       }}
+    //     />
+    //     <span className="twelve">12</span>
+    //     <span className="one">1</span>
+    //     <span className="two">2</span>
+    //     <span className="three">3</span>
+    //     <span className="four">4</span>
+    //     <span className="five">5</span>
+    //     <span className="six">6</span>
+    //     <span className="seven">7</span>
+    //     <span className="eight">8</span>
+    //     <span className="nine">9</span>
+    //     <span className="ten">10</span>
+    //     <span className="eleven">11</span>
+    //     <h3>{capital}</h3>
+    //   </div>
+    // </>
   );
 }
 
