39d0d711b163c785b7b373d22331a30cd97a6823
ThiagoAndo <ando.thiago@gmail.com>
2024-06-25 18:49:52 +0100

    Add scroll rstauration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 1=> ADD RESTORE SCROll POSITION
+
+https://timezonedb.com/references/get-time-zone => 87SX3TYK9QL5
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,53 @@
 import { Suspense } from "react";
-import {
-  useRouteLoaderData,
-  json,
-  defer,
-  Await,
-  ScrollRestoration,
-} from "react-router-dom";
-
-import { useContext } from "react";
+import { useRouteLoaderData, json, defer, Await } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 import { ClockContext } from "../store/context/clock";
 import CountryList from "../components/CountryList";
 function HomePage() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
   const { countries } = useRouteLoaderData("main");
   const context = useContext(ClockContext);
   context.stop();
 
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    let timer1 = setTimeout(
+      () => {
+        localStorage.setItem("position", scrollPosition);
+      },
+
+      200
+    );
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, [scrollPosition]);
+
+  useEffect(() => {
+    let position = localStorage.getItem("position");
+    if (!position) {
+      position = window.scrollY;
+    }
+    window.scrollTo({
+      top: position,
+    });
+    localStorage.removeItem("position");
+    setInterval(() => {
+      window.addEventListener("scroll", handleScroll, { passive: true });
+    }, 500);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
       <Await resolve={countries}>
-        {(loadedEvents) => (
-            <CountryList countries={loadedEvents} />
-        )}
+        {(loadedEvents) => <CountryList countries={loadedEvents} />}
       </Await>
     </Suspense>
   );
