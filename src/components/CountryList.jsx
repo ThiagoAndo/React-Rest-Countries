47bef785d603f81ee170e52d911229dccec99043
ThiagoAndo <ando.thiago@gmail.com>
2024-06-25 18:09:28 +0100

    Add readme

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -5,16 +5,15 @@ function CountryList({ countries }) {
   let coutrySort = countries.sort((a, b) =>
     a.name.common > b.name.common ? 1 : -1
   );
-  // const events = useLoaderData();
 
   return (
     <>
-      <Search />
-      <section id="main" className="bDark">
-        {coutrySort.map((cou) => (
-          <CountryItem key={cou.name.common} country={cou} />
-        ))}
-      </section>
+        <Search />
+        <section id="main" className="bDark">
+          {coutrySort.map((cou) => (
+            <CountryItem key={cou.name.common} country={cou} />
+          ))}
+        </section>
     </>
   );
 }
