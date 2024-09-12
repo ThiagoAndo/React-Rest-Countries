39c76b218733e876fe3ea5adecf0cdabcd0c8958
ThiagoAndo <ando.thiago@gmail.com>
2024-09-12 15:18:14 +0100

    Add 404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,27 +1,10 @@
-import { useRouteError } from "react-router-dom";
 import MainNavigation from "../components/conutryComponents/MainNavigation";
-import PageContent from "../components/conutryComponents/PageContent";
+import ErrorContent from "../components/error/errorPage";
 function ErrorPage() {
-  const error = useRouteError();
-
-  let title = "An error occurred!";
-  let message = "Something went wrong!";
-
-  if (error.status === 500) {
-    message = error.data.message;
-  }
-
-  if (error.status === 404) {
-    title = "Not found!";
-    message = "Could not find resource or page.";
-  }
-
   return (
     <>
       <MainNavigation />
-      <PageContent title={title}>
-        <p>{message}</p>
-      </PageContent>
+      <ErrorContent />
     </>
   );
 }
